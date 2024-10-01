82334900356b3eae9a9f518010da64663a39dcc7
Cynthia <kalechecynthia@gmail.com>
2024-10-01 15:17:31 +0300

    works on lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity='roller-skate') {
+    return "This Saturday, I want to " + activity + "!"
+}
+console.log (saturdayFun())
+console.log (saturdayFun(activity='bathe my dog'))
+
+function mondayWork (activity= 'go to the office') {
+    return "This Monday, I will " + activity + '.'
+}
+mondayWork(activity='work from home')
+
+
+function wrapAdjective(highlight='*') {
+    return function (feeling= 'special'){
+        return "You are " + highlight + feeling + highlight + "!"
+    }
+}
+wrapAdjective("%")("a dedicated programmer")
